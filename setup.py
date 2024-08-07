709355df38cc7f6732b22e361bade4bfbd8385d8
Amber Bagchi <amberbagchi2604@gmail.com>
2024-08-07 18:23:33 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'M ChatBot',
+    version= '0.0.0',
+    author= 'Amber Bagchi',
+    author_email= 'amberbagchi.work@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
